63c0c0203054fefcbc69ea2543b4c2f7a559f0a4
Thierry Degeling <thierry.degeling@gmail.com>
2014-01-23 16:39:13 +0100

    First running version, still no self updating :/

---
+++ src/getData.php
@@ -0,0 +1,31 @@
+<?php
+
+    //header('content-type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+
+    $apiKey = file_get_contents('apikey.txt');
+
+    $jcdecaux_stations_data = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Luxembourg&apiKey=' . $apiKey);
+
+    $stations = json_decode($jcdecaux_stations_data, true);
+
+    foreach ($stations as $station) {
+
+        $stationData['lat']     =   $station['position']['lat'];
+        $stationData['lon']     =   $station['position']['lng'];
+
+        $percentageAvailable    =   100 - round(
+            (
+                $station['available_bike_stands']
+                / $station['bike_stands']
+                * 100
+            ));
+
+        $stationData['value']   =   $percentageAvailable;
+
+
+        $heatmapdata[] = $stationData;
+
+    }
+
+    echo json_encode( $heatmapdata );
